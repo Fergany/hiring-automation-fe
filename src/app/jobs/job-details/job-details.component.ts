9f830f8dd693f60b73954ad0719cb516fe2f76c1
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-05-13 11:46:46 +0200

    edit in JobDetails design form

--- src/app/jobs/job-details/job-details.component.ts
+++ src/app/jobs/job-details/job-details.component.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute } from "@angular/router";
 export class JobDetailsComponent implements OnInit {
 
   jobId: String;
+  job: { title: string, description: string, requiredSkills: string };
 
 
   constructor(private route: ActivatedRoute) { }
@@ -16,7 +17,21 @@ export class JobDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.jobId = params.get("id")
-    })
+    });
+    this.job = {
+      title: "Software Engineer",
+      description: `Design and implement our PaaS eCommerce backend in Scala as well as our core infrastructure components like database mappings and web service APIs for high availability e-commerce
+      Analyse product requirements and discuss technical approaches
+      Test software components regarding usability, functionality and performance and work closely with Product Management and DevOps
+      Taking part in on-call rotation (with the worldwide distributed team) for production systems`,
+      requiredSkills: `Bachelor's degree (or higher) in Computer Science and/or equivalent experience/qualification and at least 4 years experience as Software Engineer + applied work experience in Scala
+      Fluent English for work in international teams
+      Deep insights in functional programming and practical work experience with Scala and in developing server-side systems using the JVM
+      Solid understanding of parallel and asynchronous programming as well as non-blocking I/O
+      Experience in developing REST APIs and knowledge of scalable architectures (incl. sharding, replication, load balancing and fail over)
+      Aspiration to constantly improve yourself and learn new technologies, concepts, etc.
+      Great team player & nice colleague who enjoys our working & company culture`
+    };
   }
 
 
