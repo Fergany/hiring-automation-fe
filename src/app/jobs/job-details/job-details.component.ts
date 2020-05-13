12f0893346a9fdf48a00fec69b47c9ddc8a23f9e
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-05-13 11:12:08 +0200

    add route to job/:id

--- src/app/jobs/job-details/job-details.component.ts
+++ src/app/jobs/job-details/job-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-job-details',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobDetailsComponent implements OnInit {
 
-  constructor() { }
+  jobId: String;
+
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.jobId = params.get("id")
+    })
   }
+
+
 
 }
