75891d441ba757f8bdb253ba30471a0b958f133a
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-05-13 04:25:25 +0200

    add component JobDetails

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JobDetailsComponent } from './job-details/job-details.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [JobDetailsComponent],
   imports: [
     CommonModule
   ]
