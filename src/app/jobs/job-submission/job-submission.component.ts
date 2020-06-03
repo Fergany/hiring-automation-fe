ac8685912514d1c48a8294303815390d55a442b1
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-06-03 09:59:57 +0200

    add JobSubmission Component

---
+++ src/app/jobs/job-submission/job-submission.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-submission',
+  templateUrl: './job-submission.component.html',
+  styleUrls: ['./job-submission.component.css']
+})
+export class JobSubmissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JobDetailsComponent } from './job-details/job-details.component';
 import { JobDetailsService } from './job-details/job-details.service';
+import { JobSubmissionComponent } from './job-submission/job-submission.component';
 
 
 @NgModule({
-  declarations: [JobDetailsComponent],
+  declarations: [JobDetailsComponent, JobSubmissionComponent],
   imports: [
     CommonModule
   ],
