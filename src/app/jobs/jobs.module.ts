6ef5addcf7802f2f6d8663b4c723a55a6927975e
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-06-08 06:37:44 +0200

    generate JobApplicaiton component

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { JobDetailsComponent } from './job-details/job-details.component';
 import { JobSubmissionComponent } from './job-submission/job-submission.component';
 import { JobApplicationsComponent } from './job-applications/job-applications.component';
+import { JobApplicationComponent } from './job-application/job-application.component';
 
 @NgModule({
-  declarations: [JobDetailsComponent, JobSubmissionComponent, JobApplicationsComponent],
+  declarations: [JobDetailsComponent, JobSubmissionComponent, JobApplicationsComponent, JobApplicationComponent],
   imports: [
     CommonModule
   ],
