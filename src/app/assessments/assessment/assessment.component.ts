b3560dd41c0453ab607c6e4f25b9baf4fe1dcaf0
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-06-28 14:38:45 +0200

    send assessment

--- src/app/assessments/assessment/assessment.component.ts
+++ src/app/assessments/assessment/assessment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AssessmentService } from './assessment.service';
 
 @Component({
   selector: 'app-assessment',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssessmentComponent implements OnInit {
 
-  constructor() { }
+  jobSubmissionId: String;
+  assessment;
+  isDataAvailable = false;
+  constructor(private route: ActivatedRoute, private assessmentService : AssessmentService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.jobSubmissionId = params.get("id");
+    });
+
+    this.assessmentService.sendAssessment(this.jobSubmissionId).subscribe(assessment => {
+      this.assessment = assessment;
+      console.log(this.assessment);
+      this.isDataAvailable = true;
+    });
   }
 
 }
--- src/app/assessments/assessment/assessment.service.ts
+++ src/app/assessments/assessment/assessment.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssessmentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendAssessment(jobSubmissionId) {
+    let headers = new HttpHeaders().set("content-type", "application/json;charset=UTF-8");
+    return this.http.post("http://localhost:8080/assessments/jobSubmission/" + jobSubmissionId,  { headers: headers });
+  }
 }
